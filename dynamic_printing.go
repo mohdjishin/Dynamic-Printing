a85ca249eafcab0270607f332ef3059cfd2ccd2d
whois-tcp <83187455+whoistcp@users.noreply.github.com>
2021-08-03 18:32:47 +0530

    Update dynamic_printing.go

--- dynamic_printing.go
+++ dynamic_printing.go
@@ -10,7 +10,7 @@ func main() {
 		i = i + 1
 		fmt.Print("\rHello World : ", i)
 		if i == 1000000 || i%1000000 == 0 {
-			fmt.Println("\ndo you wanna stop this(1 for yes 0 for No): ")
+			fmt.Println("\ndo you wanna stop this ?(1 for yes 0 for No): ")
 
 			fmt.Scanln(&a)
 			if a == 1 {
